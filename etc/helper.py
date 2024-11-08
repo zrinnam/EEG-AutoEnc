17152bff118b82050ec98c30cd899f0eae7a8ec9
Zrinna Mallock <mariazrinna@gmail.com>
2024-11-07 18:37:40 -0700

    Added comments for bounds based on what model being used

--- etc/helper.py
+++ etc/helper.py
@@ -28,6 +28,7 @@ def load_audio(filename: str) -> np.ndarray:
 
     return (audio, aud_samp_rate)
 
+##MY_MODEL_SPEC_AUDIO_TEST RUNS ON (0.264, 0.264); MY_MODEL_SPEC_TEST RUNS ON (10, 10)
 def split_events(X, events, sample_rate, bound=(0.264, 0.264)) -> np.ndarray:
     '''Split data based on sample-points. Bound should be a tuple of the bound
     around each event (eg. -100ms before and +250ms after each event should be
